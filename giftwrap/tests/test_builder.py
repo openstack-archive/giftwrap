088608bef1dacf2a4af27843cd3335de5f2adf0d
Clint Byrum <clint@fewbar.com>
2016-07-12 01:27:38 -0500

    Add unit tests for builders
    This tests the generic methods in Builder by creating a fake builder and
    using mocks to simulate requests. This actually exposed a bug in py3
    support because the file needed to be opened in binary mode.
    
    Change-Id: Ia7350ab584ba144cf0dcec9ad0f5f1ef2d830653

--- giftwrap/tests/test_builder.py
+++ giftwrap/tests/test_builder.py
@@ -16,6 +16,12 @@
 #    under the License.
 
 import copy
+import shutil
+import tempfile
+try:
+    from unittest import mock
+except:
+    import mock
 import unittest2 as unittest
 
 from giftwrap.builders import Builder, BUILDER_DRIVER_NAMESPACE
@@ -37,3 +43,77 @@ class TestBuilder(unittest.TestCase):
         base_drivers = copy.copy(BASE_DRIVERS)
         base_drivers.add('test')
         self.assertEqual(drivers, base_drivers)
+
+
+class FakeBuilder(Builder):
+
+    def __del__(self):
+        try:
+            if self._temp_src_dir:
+                shutil.rmdir(self._temp_src_dir)
+        except Exception:
+            pass
+        try:
+            if self._temp_dir:
+                shutil.rmdir(self._temp_src_dir)
+        except Exception:
+            pass
+
+    def _execute(self, command, cwd=None, exit=0):
+        return
+
+    def _make_temp_dir(self, prefix='giftwrap'):
+        return tempfile.mkdtemp()
+
+    def _make_dir(self, path, mode=0o777):
+        return
+
+    def _prepare_build(self):
+        return
+
+    def _prepare_project_build(self, project):
+        return
+
+    def _clone_project(self, project):
+        return
+
+    def _create_virtualenv(self, venv_command, path):
+        return
+
+    def _install_pip_dependencies(self, venv_path, dependencies,
+                                  use_constraints=True):
+        return
+
+    def _copy_sample_config(self, src_clone_dir, project):
+        return
+
+    def _install_project(self, venv_path, src_clone_dir):
+        return
+
+    def _finalize_project_build(self, project):
+        return
+
+    def _finalize_build(self):
+        return
+
+    def _cleanup_build(self):
+        return
+
+
+class TestBuilderBuilds(unittest.TestCase):
+
+    @mock.patch('requests.get')
+    def test_build(self, requests_mock):
+        response = mock.MagicMock('response')
+        response.raise_for_status = mock.MagicMock('raise_for_status')
+        response.text = '# no constraints\n'
+        requests_mock.return_value = response
+        spec = mock.MagicMock('spec')
+        spec.settings = mock.MagicMock('settings')
+        spec.settings.constraints = ['http://noexist.test/constraints.txt']
+        spec.settings.parallel_build = False
+        spec.projects = []
+        x = FakeBuilder(spec)
+        x.build()
+        requests_mock.assert_called_once_with(
+            'http://noexist.test/constraints.txt')
